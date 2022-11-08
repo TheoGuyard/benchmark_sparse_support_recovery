23abbc745bac7c83aad0d7d5c2c4e68ca9300838
Theo Guyard <theo.guyard@insa-rennes.fr>
2022-11-08 17:34:53 +0100

    Modify synthetic data

--- datasets/simulated.py
+++ datasets/simulated.py
@@ -7,18 +7,24 @@ class Dataset(BaseDataset):
 
     name = "Simulated"
 
-    parameters = {"n_samples, n_features": [(100, 300), (300, 100)]}
+    parameters = {"n_samples, n_features, n_nnz": [(100, 300, 5), (300, 100, 5)]}
 
-    def __init__(self, n_samples=10, n_features=50, random_state=27):
+    def __init__(self, n_samples=10, n_features=50, n_nnz=2, random_state=27):
         self.n_samples = n_samples
         self.n_features = n_features
+        self.n_nnz = n_nnz
         self.random_state = random_state
 
     def get_data(self):
         rng = np.random.RandomState(self.random_state)
+        w = np.zeros(self.n_features)
+        s = np.linspace(0, self.n_features-1, num=self.n_nnz).round().astype(int)
+        w[s] = np.sign(rng.randn(self.n_nnz))
         X = rng.randn(self.n_samples, self.n_features)
-        y = rng.randn(self.n_samples)
         X /= np.linalg.norm(X, 2, axis=0)
-        y /= np.linalg.norm(y, 2)
+        y = X @ w
+        e = rng.randn(self.n_samples)
+        e *= np.sqrt(w.T @ w) / np.sqrt(10. * (e.T @ e))
+        y += e
         M = 1.0
         return dict(X=X, y=y, M=M)
