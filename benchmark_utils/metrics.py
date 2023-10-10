454cbcff8d665bb8b85aa5a1f4cc4fbc84d9b032
Theo Guyard <theo.guyard@insa-rennes.fr>
2023-10-10 11:01:08 +0200

    Lint

--- benchmark_utils/metrics.py
+++ benchmark_utils/metrics.py
@@ -1,10 +1,13 @@
 import numpy as np
 
+
 def snr(w_true, w):
     return np.linalg.norm(w_true, 2) / np.linalg.norm(w_true - w, 2)
 
+
 def fpr(w_true, w):
     return np.sum((w_true == 0.0) * (w != 0.0)) / np.sum(w_true != 0.0)
+
 
 def fnr(w_true, w):
     return np.sum((w_true != 0.0) * (w == 0.0)) / np.sum(w_true == 0.0)
