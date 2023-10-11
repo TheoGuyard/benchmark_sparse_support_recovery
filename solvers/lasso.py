3d8befe59c770eb31ef5a2abdc534752aaa6a49d
Matthieu Kowalski <matthieu.kowalski@u-psud.fr>
2023-10-11 14:08:01 +0200

    flake8

--- solvers/lasso.py
+++ solvers/lasso.py
@@ -19,25 +19,23 @@ class Solver(BaseSolver):
     def run(self, iteration):
         start_time = time.time()
         k = int(np.floor(iteration * self.X.shape[0]))
-        alphaMax = np.linalg.norm(self.X.T@self.y,np.inf)/self.y.shape[0]
+        alphaMax = np.linalg.norm(self.X.T@self.y, np.inf)/self.y.shape[0]
         alphaMin = alphaMax*1e-15
         w = np.zeros(self.X.shape[1])
         if k > 0:
-            for lamb in np.logspace(alphaMax,alphaMin,1000):
+            for lamb in np.logspace(alphaMax, alphaMin, 1000):
                 wold = w
-                solver = Lasso(alpha=lamb,warm_start=True,fit_intercept=False,max_iter=10000)
+                solver = Lasso(alpha=lamb, warm_start=True, fit_intercept=False, max_iter=10000)
                 with warnings.catch_warnings():
                     warnings.simplefilter("ignore")
                     solver.fit(self.X, self.y)
                 w = solver.coef_.flatten()
                 if np.sum(w != 0) > k:
                     w = wold
-                    break
-                
+                    break      
         self.k = k
         self.w = w
         self.solve_time = time.time() - start_time
-                    
 
     def get_result(self):
         return dict(k=self.k, w=self.w, solve_time=self.solve_time)
